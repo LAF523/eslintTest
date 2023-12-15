2bfc5358bf7364d214bf770e532d3a8684676129
aofei.li <aofei.li@pacteraedge.com>
2023-12-15 21:09:11 +0800

    测试

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteEslint from 'vite-plugin-eslint';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    viteEslint({
+      // failOnError: false
+    })
+  ],
+  css: {
+    // 预处理器配置项
+    preprocessorOptions: {
+      less: {
+        math: 'always',
+        globalVars: {
+          //配置全局变量
+          blue: '#1CC0FF'
+        }
+      }
+    }
+  }
+});
