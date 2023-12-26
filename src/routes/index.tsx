6d03906a1e22ebb57582add75d9cc481062dd1f2
aofei.li <aofei.li@pacteraedge.com>
2023-12-26 18:47:01 +0800

    feat: 设置路由基础路径

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
 import { createBrowserRouter } from 'react-router-dom';
 import router from './baseRouter';
 
-export default createBrowserRouter(router);
+export default createBrowserRouter(router, { basename: '/eslintTest/' });
