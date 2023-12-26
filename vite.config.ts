a45f462a979070ccc4c5de3e635971650d59f106
aofei.li <aofei.li@pacteraedge.com>
2023-12-27 00:31:44 +0800

    feat: kkk

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react';
 import viteEslint from 'vite-plugin-eslint';
 import path from 'path';
 
-// console.log(import.meta.env.VITE_BASE_URL)
-console.log(process.env.TEST);
-
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/eslintTest/',
