85370ac797b803291e6914b5b10eb364bff6c8ca
aofei.li <aofei.li@pacteraedge.com>
2023-12-26 23:27:51 +0800

    feat: 简洁

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
 import { createBrowserRouter } from 'react-router-dom';
 import router from './baseRouter';
 
-export default createBrowserRouter(router, { basename: '/eslintTest/' });
+export default createBrowserRouter(router);
