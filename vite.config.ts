616fc4b63098acfcf44bcc91a313eee15bac396c
aofei.li <aofei.li@pacteraedge.com>
2023-12-28 17:30:34 +0800

    feat: llll

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: 'eslintTest',
+  base: 'eslintTest',
   resolve: {
     extensions: ['.js', '.ts', '.tsx', '.scss', '.css'],
     alias: {
